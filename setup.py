596b54fcfc8f5b99a5e28aafab534ff336c4eabf
F. Oliver Gathmann <fogathmann@gmail.com>
2013-09-04 10:53:39 +0200

    Set TheLMA version to 1.6.2 in the v16x branch

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ tests_requirements = install_requirements + [
     ]
 
 setup(name='TheLMA',
-      version='1.6.1',
+      version='1.6.2',
       description='TheLMA',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
