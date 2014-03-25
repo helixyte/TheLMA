3c2d59c57091714e8d6199ebf6d2e6605c6e4fe6
F. Oliver Gathmann <fogathmann@gmail.com>
2014-03-25 15:20:05 +0100

    Fixed attachment markup in Trac reports (Fixes #5477).

--- thelma/automation/tools/iso/tracreporting.py
+++ thelma/automation/tools/iso/tracreporting.py
@@ -36,7 +36,7 @@ class IsoStockTransferReporter(BaseTracTool):
 
     #: The ticket comment for the update.
     BASE_COMMENT = 'A stock transfer has been executed by %s ' \
-                   '(see file: attachment:%s).[[br]]' \
+                   '(see file: [attachment:%s]).[[br]]' \
                    "'''Entity:''' %s[[br]]" \
                    "'''Type:''' %s[[br]]" \
                    "%s.[[br]]"
