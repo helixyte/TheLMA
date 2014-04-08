907e2d87c8d132d574d24de63b7b989e0f64e1e2
F. Oliver Gathmann <fogathmann@gmail.com>
2014-04-08 08:18:11 +0200

    Changed version to 1.7.4

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ tests_requirements = install_requirements + [
     ]
 
 setup(name='TheLMA',
-      version='1.7.3',
+      version='1.7.4',
       description='TheLMA',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
