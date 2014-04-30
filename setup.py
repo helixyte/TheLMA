658ad1fddd8dde7cb610ac1dc8a3af15e2991588
F. Oliver Gathmann <fogathmann@gmail.com>
2014-04-30 17:07:49 +0200

    Setting TheLMA v17x version to 1.7.6

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ tests_requirements = install_requirements + [
     ]
 
 setup(name='TheLMA',
-      version='1.7.5',
+      version='1.7.6',
       description='TheLMA',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
