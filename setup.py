16bab1ade245979a35cbd3bcf384a433aa255cad
F. Oliver Gathmann <fogathmann@gmail.com>
2014-09-15 14:16:29 +0200

    Setting the production version to 1.7.17

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ tests_requirements = install_requirements + [
     ]
 
 setup(name='TheLMA',
-      version='1.7.16',
+      version='1.7.17',
       description='TheLMA',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
